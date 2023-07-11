46bbbb3d24540bbc38dabaf622e9c3dd4d222059
Apurva Muthe <108541134+MutheApurva@users.noreply.github.com>
2023-07-11 20:18:35 +0100

    Committed

---
+++ README.md
@@ -0,0 +1,4 @@
+# Personal Homepage
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/MutheApurva/pen/LYJqJgo](https://codepen.io/MutheApurva/pen/LYJqJgo).
+
---
+++ script.js
@@ -0,0 +1,95 @@
+/* =================
+  TESTS, LOOK AT THESE
+  Reading tests will always help you discover your requirements.
+  You can make this window bigger. 
+   ===================
+*/
+document
+  .getElementById("comment-submit-button")
+  .addEventListener("click", (event) => {
+    let element_comment_input = document.getElementById("comment-input");
+    let new_li = document.createElement("li");
+    let element_comments_container = document.getElementById(
+      "comments-container"
+    );
+    new_li.innerText = element_comment_input.value;
+
+    new_li.classList.add("custom-font"); // Apply the custom class to the new li element
+
+    element_comments_container.appendChild(new_li);
+  });
+
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
+
+const {
+  core: { test, expect, run },
+  prettify
+} = window.jestLite;
+
+/* =================
+  FIND ELEMENTS
+  These are all the elements we will look for.
+   ===================
+*/
+const getHeader = document.querySelectorAll("header"),
+  getH1 = document.querySelectorAll("h1"),
+  getSiteHeader = document.querySelectorAll(".c-site-header"),
+  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
+  getMain = document.querySelectorAll("main"),
+  getFooter = document.querySelectorAll("footer"),
+  getSiteFooter = document.querySelectorAll(".c-site-footer"),
+  getIFrame = document.querySelectorAll("iframe"),
+  getImage = document.querySelectorAll("img"),
+  getWords = document.body.innerText;
+
+/* =================
+   ASSERTIONS 
+   These are the things we check are true about your page.
+   Read and update your HTML to discover the requirements.
+   The tests will run every time you update your code.
+   ===================
+*/
+test("There is at least one header element", () => {
+  expect(getHeader.length).toBeGreaterThanOrEqual(1);
+});
+test("There is at least one h1", () => {
+  expect(getH1.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one header element with the class c-site-header", () => {
+  expect(getSiteHeader.length).toBe(1);
+});
+test("There is a nav element with an aria-label of Main Site Links.", () => {
+  expect(getAria.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one main element", () => {
+  expect(getMain.length).toBe(1);
+});
+test("There is at least one footer element", () => {
+  expect(getFooter.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one footer element with the class c-site-footer", () => {
+  expect(getSiteFooter.length).toBe(1);
+});
+test("There is embedded video", () => {
+  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
+});
+test("There is at least one image", () => {
+  expect(getImage.length).toBeGreaterThanOrEqual(1);
+});
+test("There are at least 500 words on the page", () => {
+  expect(getWords.length).toBeGreaterThanOrEqual(500);
+});
+
+const console = document.getElementById("tests");
+prettify.toHTML(run(), console);
